c1fd10e02f58e122bfcc7e4187e81ef4baa9cb77
daver234 <dave.rothschild@gmail.com>
2016-04-18 21:27:05 -0700

    some updates
    a few more things

--- SwiftPullToRefresh/RefreshItem.swift
+++ SwiftPullToRefresh/RefreshItem.swift
@@ -30,8 +30,7 @@ class RefreshItem {
   init(view: UIView, centerEnd: CGPoint, parallaxRatio: CGFloat, sceneHeight: CGFloat) {
     self.view = view
     self.centerEnd = centerEnd
-    centerStart = CGPoint(x: centerEnd.x,
-      y: centerEnd.y + (parallaxRatio * sceneHeight))
+    centerStart = CGPoint(x: centerEnd.x, y: centerEnd.y + (parallaxRatio * sceneHeight))
     self.view.center = centerStart
   }
 
